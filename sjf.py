239c0ac6035f12b1ea59b2d627c95d745c281a68
Franciszek <franciszekplisz@protonmail.com>
2023-01-13 03:12:07 +0100

    added sjf solution

--- sjf.py
+++ sjf.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
 #-*-coding:UTF-8-*-
 
-import queue
+data = [[2,6],[5,2],[1,8],[0,3],[4,4]] # arrival timer, burst timer
+ 
+def sjf_sim(data, numberOfProcesses):
+    timer = 0
+    queue = []
+    completed = []
+    
+    while True:
+        if len(completed) == numberOfProcesses:
+            return completed
+        i = 0
+        while i < len(data):
+            if data[i][0] <= timer:
+                queue.append(data[i])
+                del data[i]
+            else:
+                i += 1
+        queue = sorted(queue, key=lambda x: x[1])  # sort by bursttimer
+        if len(queue)!=0:
+            current_process = queue[0]
+            completion_timer = current_process[1] + timer
+            completed.append((current_process[0],current_process[1], completion_timer))
+            timer = current_process[1] + timer
+            queue.pop(0)
+    
+        else:
+            timer+=1
 
-class Process:
-    def __init__(self, pid, burst_time, arrival_time):
-        self.pid = pid
-        self.burst_time = burst_time
-        self.arrival_time = arrival_time
-        self.waiting_time = 0
+def avgWaitingTime(data):
+    sum = 0
+    for i in data:
+        print(i)
+        sum = i[2]-i[1]-i[0] + sum
+    return sum/len(data)
+        
 
-def calculate_avg_waiting_time(processes, n):
-    waiting_time = 0
-    for i in range(n):
-        waiting_time += processes[i].waiting_time
-    return waiting_time / n
-
-def SJF_scheduling(process_list, num_processes):
-    q = queue.PriorityQueue()
-    current_time = 0
-    waiting_time = 0
-    for i in range(num_processes):
-        q.put((process_list[i].arrival_time, process_list[i].burst_time, process_list[i]))
-
-    while not q.empty():
-        current_process = q.get()[2]
-        current_time += current_process.burst_time
-        current_process.waiting_time = current_time - current_process.arrival_time - current_process.burst_time
-        print("Zadanie", current_process.pid, "rozpoczyna się o czasie", current_time)
-
-    avg_waiting_time = calculate_avg_waiting_time(process_list, num_processes)
-    print("Średni czas oczekiwania:", avg_waiting_time)
-
-# przykładowe zadania
-process_list = [Process(1, 20, 2), Process(2, 8, 6), Process(3, 4, 6)]
-num_processes = 3
-
-# uruchamiamy algorytm
-SJF_scheduling(process_list, num_processes)
+sol = sjf_sim(data,len(data))
+print(len(sol))
+print(avgWaitingTime(sol))
